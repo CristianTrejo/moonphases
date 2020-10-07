b9e84164e27ccf6a537bebaa247b7e186922bf1e
CristianTrejo <cristian.trejo.t@hotmail.com>
2020-10-07 12:30:21 -0600

    Controller structure

---
+++ app/Controllers/Http/MoonphaseController.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class MoonphaseController {
+    getMontMoonPhases({params, response}){}
+
+    getNewMoons({params, response}){}
+
+    getFullMoons({params, response}) {}
+}
+
+module.exports = MoonphaseController
